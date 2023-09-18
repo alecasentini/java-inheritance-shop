797f468ec1ce29602bdf259ac27110980607277b
alecasentini <ale.casentini@gmail.com>
2023-09-18 16:06:09 +0200

    Creazione classe Prodotto e sottoclassi

---
+++ src/org/java/shop/Prodotto.java
@@ -0,0 +1,70 @@
+package org.java.shop;
+
+public class Prodotto {
+	private String codice;
+	private String nome;
+	private String marca;
+	private float prezzo;
+	private int iva;
+	
+	public Prodotto (String codice, String nome, String marca, float prezzo, int iva){
+		this.codice = codice;
+		setNome(nome);
+		setMarca(marca);
+		setPrezzo (prezzo);
+		setIva (iva);
+	}
+	
+	public String getCodice() {
+		
+		return codice;
+	}
+	
+	public String getNome() {
+		
+		return nome;
+	}
+	public void setNome(String nome) {
+		
+		this.nome = nome;
+	}
+	
+	public String getMarca() {
+		
+		return marca;
+	}
+	public void setMarca(String marca) {
+		
+		this.marca = marca;
+	}
+	
+	public float getPrezzo() {
+		
+		return prezzo;
+	}
+	public void setPrezzo(float prezzo) {
+		
+		this.prezzo = prezzo;
+	}
+	
+	public int getIva() {
+		
+		return iva;
+	}
+	public void setIva(int iva) {
+		
+		this.iva = iva;
+	}
+	
+	public float getPrezzoBase() {
+		return prezzo;
+	}
+	
+	public String getPrezzoIva() {
+		float prezzoIva = prezzo + (prezzo * iva / 100);
+		return String.format("%.2f", prezzoIva);
+	}
+	
+	
+};
+
